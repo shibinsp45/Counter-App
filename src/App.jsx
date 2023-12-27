f13b3592123b46fe6ff89630326c0ac9e5c540de
Shibin S P <shibinsp45@gmail.com>
2023-12-27 12:14:15 +0530

    app completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from "./assets/Components/Counter";
+
+function App() {
+  
+  return (
+    <>
+      <div style={{height:'100vh'}} className='w-100 d-flex justify-content-center align-items-center '>
+        <Counter/>
+      </div>
+    </>
+  );
+}
+
+export default App;
